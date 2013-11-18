d402c8b22fd58cabedf519df57aa9c05c2c4b855
johntalent <talent.corp@gmail.com>
2013-11-18 09:44:47 -0500

    Mejora del javascript scripts

--- turtle-develop/js/scripts.js
+++ turtle-develop/js/scripts.js
@@ -1,73 +1,74 @@
-/*-----------------------------------------------------------------------------------*/
-/*	BUTTON HOVER
-/*-----------------------------------------------------------------------------------*/
-
 jQuery(document).ready(function($)  {
-	$("a.button, .forms fieldset .btn-submit, #commentform input#submit").css("opacity","1.0");
-	$("a.button, .forms fieldset .btn-submit, #commentform input#submit").hover(
-		function () {
-			$(this).stop().animate({ opacity: 0.9 }, "fast");  
-		},
-		function () {
-			$(this).stop().animate({ opacity: 1.0 }, "fast");  
-	});
+	jQuery.fn.exists = function(){return this.length>0;}
 	
-	$('#runButton').click(function(){
-		Turtle.runButtonClick();
-		return false;
-	});
-	$('#runStepButton').click(function(){
-		Turtle.runStepButtonClick();
-		return false;
-	});
-	$('#linkButton').click(function(){
+	if($('.full-wrapper').exists()){
 		
-		return false;
-	});
-	$('#viewCode').click(function(){
+		$("a.button, .forms fieldset .btn-submit, #commentform input#submit").css("opacity","1.0");
+		$("a.button, .forms fieldset .btn-submit, #commentform input#submit").hover(
+			function () {
+				$(this).stop().animate({ opacity: 0.9 }, "fast");  
+			},
+			function () {
+				$(this).stop().animate({ opacity: 1.0 }, "fast");  
+		});
 		
-		return false;
-	});
-	$('#resetButton').click(function(){
-		Turtle.resetButtonClick();
-		return false;
-	});
-	
-	Blockly.resizeBlockly = function(){
-		window.width_old = 0;
-  		window.height_old = 0;
-		Turtle.onresize();
-		Blockly.svgResize();
-		Blockly.Toolbox.position_();
-		Blockly.mainWorkspace.trashcan.position_()
-	};
-	
-	$('#controls-1-1').click(function(){
-		$('#control-left').removeClass( 'one-half one-third two-third');
-		$('#control-right').removeClass('one-half one-third two-third last');
-		$('#control-left').addClass('one-half');
-		$('#control-right').addClass('one-half last');
-		Blockly.resizeBlockly();
-		return false;
-	}); 
-	
-	$('#controls-1-2').click(function(){
-		$('#control-left').removeClass( 'one-half one-third two-third');
-		$('#control-right').removeClass('one-half one-third two-third last');
-		$('#control-left').addClass('one-third');
-		$('#control-right').addClass('two-third last');
-		Blockly.resizeBlockly();
-		return false;
-	});
-	
-	$('#controls-2-1').click(function(){
-		$('#control-left').removeClass( 'one-half one-third two-third');
-		$('#control-right').removeClass('one-half one-third two-third last');
-		$('#control-left').addClass('two-third');
-		$('#control-right').addClass('one-third last');
-		Blockly.resizeBlockly();
-		return false;
-	});
+		$('#runButton').click(function(){
+			Turtle.runButtonClick();
+			return false;
+		});
+		$('#runStepButton').click(function(){
+			Turtle.runStepButtonClick();
+			return false;
+		});
+		$('#linkButton').click(function(){
+			
+			return false;
+		});
+		$('#viewCode').click(function(){
+			
+			return false;
+		});
+		$('#resetButton').click(function(){
+			Turtle.resetButtonClick();
+			return false;
+		});
+		
+		Blockly.resizeBlockly = function(){
+			window.width_old = 0;
+	  		window.height_old = 0;
+			Turtle.onresize();
+			Blockly.svgResize();
+			Blockly.Toolbox.position_();
+			Blockly.mainWorkspace.trashcan.position_()
+		};
+		
+		$('#controls-1-1').click(function(){
+			$('#control-left').removeClass( 'one-half one-third two-third');
+			$('#control-right').removeClass('one-half one-third two-third last');
+			$('#control-left').addClass('one-half');
+			$('#control-right').addClass('one-half last');
+			Blockly.resizeBlockly();
+			return false;
+		}); 
+		
+		$('#controls-1-2').click(function(){
+			$('#control-left').removeClass( 'one-half one-third two-third');
+			$('#control-right').removeClass('one-half one-third two-third last');
+			$('#control-left').addClass('one-third');
+			$('#control-right').addClass('two-third last');
+			Blockly.resizeBlockly();
+			return false;
+		});
+		
+		$('#controls-2-1').click(function(){
+			$('#control-left').removeClass( 'one-half one-third two-third');
+			$('#control-right').removeClass('one-half one-third two-third last');
+			$('#control-left').addClass('two-third');
+			$('#control-right').addClass('one-third last');
+			Blockly.resizeBlockly();
+			return false;
+		});
+	}
 	
 });
 
