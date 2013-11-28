36b693d72f892c1a139435d1e0dd2253098c76b7
Rafael Laverde <leafar91@gmail.com>
2013-11-27 22:04:04 -0500

    Script automatizar backup, haciendo peticiones al servicio backup

---
+++ backup/automatization_backup.py
@@ -0,0 +1,18 @@
+import requests
+import time
+url='http://localhost/backup'
+#url='phi.uis.edu.co/backup'
+
+data= {
+    'make_backup':'12334421'
+    }
+
+current_time = time.strftime("%d-%m %I:%M")
+f = open(current_time, "w")
+f.write(requests.post(url, data=data).text)
+
+
+
+
+
+ 
