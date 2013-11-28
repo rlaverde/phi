d188ace62c702c0770c6810eac8490bcba0f2deb
Rafael Laverde <leafar91@gmail.com>
2013-11-28 08:49:44 -0500

    corrección error hora: cambio a 24 horas

--- backup/automatization_backup.py
+++ backup/automatization_backup.py
@@ -1,20 +1,14 @@
 import requests
 import time
 #url='http://localhost/backup'
-url='http://phi.uis.edu.co/backup'
+url = 'http://phi.uis.edu.co/backup'
 
-data= {
-    'make_backup':'12334421'
+data = {
+    'make_backup': '12334421'
     }
 
-current_time = time.strftime("%d-%m %I:%M")
+current_time = time.strftime("%d-%m %H:%M")
 f = open("/root/backups/{}".format(current_time), "w")
 #f = open(current_time, "w")
 f.write(requests.post(url, data=data).content)
 f.close
-
-
-
-
-
- 
